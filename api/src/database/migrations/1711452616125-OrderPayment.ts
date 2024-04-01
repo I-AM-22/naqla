073e7cdf6b0eea78e40d549f5693d6593b335e93
bahaa-alden <samerabood195@gmail.com>
2024-04-01 13:32:27 +0300

    [api]:fix for majed

--- api/src/database/migrations/1711452616125-OrderPayment.ts
+++ api/src/database/migrations/1711452616125-OrderPayment.ts
@@ -5,9 +5,7 @@ export class OrderPayment1711452616125 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "payments" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "total" integer DEFAULT '0', "additionalCost" integer DEFAULT '0', "deliveredDate" TIMESTAMP, "orderId" uuid NOT NULL, CONSTRAINT "REL_af929a5f2a400fdb6913b4967e" UNIQUE ("orderId"), CONSTRAINT "PK_197ab7af18c93fbb0c9b28b4a59" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "receiving_date"`);
         await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "cost"`);
-        await queryRunner.query(`ALTER TABLE "orders" ADD "desiredDate" TIMESTAMP NOT NULL`);
         await queryRunner.query(`ALTER TABLE "orders" ADD "paymentId" uuid NOT NULL`);
         await queryRunner.query(`ALTER TABLE "orders" ADD CONSTRAINT "UQ_06a051324c76276ca2a9d1feb08" UNIQUE ("paymentId")`);
         await queryRunner.query(`ALTER TABLE "payments" ADD CONSTRAINT "FK_af929a5f2a400fdb6913b4967e1" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
