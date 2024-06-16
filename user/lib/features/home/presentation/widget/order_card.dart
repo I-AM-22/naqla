1a6bfda32725f42ce0e0e42ef62781bc2b05105c
majedAlaswad <alaswadmajed389@gmail.com>
2024-06-16 19:01:23 +0300

    refactor(user): refactor order card ui

--- user/lib/features/home/presentation/widget/order_card.dart
+++ user/lib/features/home/presentation/widget/order_card.dart
@@ -2,61 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:naqla/core/core.dart';
+import 'package:naqla/features/home/presentation/widget/order_status_indicator.dart';
 
 import '../../../../core/util/core_helper_functions.dart';
 import '../../../../generated/l10n.dart';
 import '../../data/model/order_model.dart';
 
-class OrderCard extends StatelessWidget {
+class OrderCard extends StatefulWidget {
   const OrderCard({super.key, required this.orderModel});
   final OrderModel orderModel;
 
   @override
+  State<OrderCard> createState() => _OrderCardState();
+}
+
+class _OrderCardState extends State<OrderCard> with SingleTickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
     return Container(
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      decoration: BoxDecoration(border: Border.all(color: context.colorScheme.primary), borderRadius: BorderRadius.circular(8)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      decoration: BoxDecoration(
+        border: Border.all(color: context.colorScheme.primary),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                10.verticalSpace,
-                AppText.bodySmMedium('${S.of(context).order_date} ${CoreHelperFunctions.fromDateTimeToString(orderModel.desiredDate)}'),
-                10.verticalSpace,
-                AppText.bodySmMedium('${S.of(context).order_status} ${orderModel.status.name}'),
-                10.verticalSpace,
-                AppText.bodySmMedium('${S.of(context).cost} ${orderModel.paymentModel?.cost} ${S.of(context).syp}'),
-              ],
-            ),
-          ),
-          // const Spacer(),
-          Stack(
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              SizedBox(
-                  height: 150.h,
-                  width: 150.w,
-                  child: BlurHash(imageFit: BoxFit.cover, hash: orderModel.photos[0].blurHash, image: orderModel.photos[0].mobileUrl)),
-              if (orderModel.photos.length > 1)
-                Container(
-                  color: context.colorScheme.primary.withOpacity(.5),
-                  child: SizedBox(
-                    width: 150.w,
-                    height: 150.w,
-                    child: Center(
-                        child: AppText.titleMedium(
-                      '+${orderModel.photos.length - 1}',
-                      color: Colors.white,
-                    )),
-                  ),
-                )
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    10.verticalSpace,
+                    AppText.bodySmMedium('${S.of(context).order_date} ${CoreHelperFunctions.fromDateTimeToString(widget.orderModel.desiredDate)}'),
+                    10.verticalSpace,
+                    AppText.bodySmMedium('${S.of(context).order_status} ${widget.orderModel.status.name}'),
+                    10.verticalSpace,
+                    AppText.bodySmMedium('${S.of(context).cost} ${widget.orderModel.paymentModel?.cost} ${S.of(context).syp}'),
+                  ],
+                ),
+              ),
+              // const Spacer(),
+              Stack(
+                children: [
+                  SizedBox(
+                      height: 150.h,
+                      width: 150.w,
+                      child:
+                          BlurHash(imageFit: BoxFit.cover, hash: widget.orderModel.photos[0].blurHash, image: widget.orderModel.photos[0].mobileUrl)),
+                  if (widget.orderModel.photos.length > 1)
+                    Container(
+                      color: context.colorScheme.primary.withOpacity(.5),
+                      child: SizedBox(
+                        width: 150.w,
+                        height: 150.w,
+                        child: Center(
+                            child: AppText.titleMedium(
+                          '+${widget.orderModel.photos.length - 1}',
+                          color: Colors.white,
+                        )),
+                      ),
+                    )
+                ],
+              )
             ],
-          )
+          ),
+          OrderStatusIndicator()
         ],
       ),
     );
--- user/lib/features/home/presentation/widget/order_status_indicator.dart
+++ user/lib/features/home/presentation/widget/order_status_indicator.dart
@@ -3,8 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:naqla/core/core.dart';
 
 class OrderStatusIndicator extends StatefulWidget {
-  final int currentIndex;
-  const OrderStatusIndicator({super.key, required this.currentIndex});
+  const OrderStatusIndicator({super.key});
 
   @override
   State<OrderStatusIndicator> createState() => _OrderStatusIndicatorState();
@@ -28,39 +27,31 @@ class _OrderStatusIndicatorState extends State<OrderStatusIndicator> with Single
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: List.generate(
-        4,
-        (index) => Expanded(
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 500),
-            child: Container(
-              decoration: BoxDecoration(
-                color: (index + 1 <= widget.currentIndex || widget.currentIndex == 3) ? context.colorScheme.primary : Colors.white.withOpacity(.4),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              height: 4.h,
-              margin: REdgeInsetsDirectional.only(end: index != 4 ? 8 : 0),
-              child: AnimatedOpacity(
-                duration: const Duration(milliseconds: 500),
-                opacity: (index == widget.currentIndex && widget.currentIndex != 3) ? 1 : 0,
-                child: AnimatedBuilder(
-                  animation: _animationController,
-                  builder: (context, child) {
-                    return Transform.scale(
-                      scaleX: _animationController.value,
-                      alignment: _animationController.status == AnimationStatus.reverse ? Alignment.centerRight : Alignment.centerLeft,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: context.colorScheme.primary,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    );
-                  },
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 500),
+      child: Container(
+        decoration: BoxDecoration(
+          color: context.colorScheme.waiting,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        height: 4.h,
+        child: AnimatedOpacity(
+          duration: const Duration(milliseconds: 500),
+          opacity: 1,
+          child: AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) {
+              return Transform.scale(
+                scaleX: _animationController.value,
+                alignment: _animationController.status == AnimationStatus.reverse ? Alignment.centerRight : Alignment.centerLeft,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: context.colorScheme.primary,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
                 ),
-              ),
-            ),
+              );
+            },
           ),
         ),
       ),
