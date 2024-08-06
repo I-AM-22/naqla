be2c038147408535a447e3717f04deaa28b7c9d3
bahaa alden abdo <samerabood195@gmail.com>
2024-08-06 11:25:38 +0000

    [API]:Update auth-user.service.ts

--- api/src/auth-user/services/auth-user.service.ts
+++ api/src/auth-user/services/auth-user.service.ts
@@ -91,7 +91,7 @@ export class AuthUserService implements IAuthUserService {
       throw new UnauthorizedException(incorrect_credentials_OTP);
     }
     const nonConfirmedUser = await this.usersService.findOne(otp.userId);
-    if (!nonConfirmedUser || nonConfirmedUser.disactiveAt !== null) {
+    if (!nonConfirmedUser) {
       throw new UnauthorizedException(incorrect_credentials_OTP);
     }
 
