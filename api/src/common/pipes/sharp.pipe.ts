db249032ca1093261cb2cf45f67a8e84be9a7bed
bahaa-alden <samerabood195@gmail.com>
2024-09-25 04:07:08 +0300

    feat: add vercel

--- api/src/common/pipes/sharp.pipe.ts
+++ api/src/common/pipes/sharp.pipe.ts
@@ -1,9 +1,12 @@
-import { Injectable, PipeTransform } from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 import { customize } from '../helpers';
 
 @Injectable()
 export class SharpPipe implements PipeTransform<Express.Multer.File, Promise<string | string[]>> {
   async transform(image: Express.Multer.File & { photos: Express.Multer.File[] }): Promise<string | string[]> {
+    if (!image) {
+      throw new BadRequestException('Please upload at least one photo');
+    }
     if (Array.isArray(image.photos)) {
       const { photos }: { photos: Express.Multer.File[] } = image;
       return Promise.all(photos.map((e) => customize(e)));
